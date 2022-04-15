f5cdadde6bf12aa03dc06042b9c6fa3b1a5dec84
Monique Gouveia <90391598+moniquegouveia@users.noreply.github.com>
2022-04-15 10:54:40 +0100

    Update run.py according to PEP8

--- run.py
+++ run.py
@@ -21,36 +21,31 @@ def hangmanGame():
     letters in the alphabet'''
     usedLetters = set()  # creates a set of the letters already used
 
- lives = 7  
- ''' sets the number of lives to 7 
-  (includes the head, the body, the left arm, 
+    lives = 7
+''' sets the number of lives to 7
+(includes the head, the body, the left arm,
   the right arm, the left leg, and the right leg) '''
-  
-  while len(wordLetters) > 0 and lives > 0:  # while the word has letters and the user still has lives
+
+while len(wordLetters) > 0 and lives > 0:
     print("\n")  # prints a new line
-    print("Lives Remaining: " + str(lives))  # prints the number of lives remaining
-    
-        userLetter = input("\nGuess a letter: ").upper() #asks the user to enter a letter (returns in uppercase)   
-    if userLetter in alphabet - usedLetters:
-      usedLetters.add(userLetter)  # adds the letter to the used letters set
-    if userLetter in wordLetters:
-        wordLetters.remove(userLetter)  # removes the letter from the word letters set
-        print("\nNice Guess!")
-            else:
-        lives -= 1  # subtracts a life for incorrect guess
-        print("\nAhh, you lost a life. Try again!")
-            elif userLetter in usedLetters:
-      print("\nYou have already guessed that letter! Guess a different one.")  
-      # tells the user they have already guessed that letter
-    else:
-      print("\nThat is not a letter! Guess a different one.")  
-      # prints an error message if the user enters a letter that is not in the alphabet
-      
-       if lives == 0:  # marks the end of the game
+    print("Lives Remaining: " + str(lives))
+userLetter = input("\nGuess a letter: ").upper()
+if userLetter in alphabet - usedLetters:
+    usedLetters.add(userLetter)  # adds the letter to the used letters set
+if userLetter in wordLetters:
+    wordLetters.remove(userLetter)
+print("\nNice Guess!")
+else:
+    lives -= 1  # subtracts a life for incorrect guess
+    print("\nAhh, you lost a life. Try again!")
+elif userLetter in usedLetters:
+    print("\nYou have already guessed that letter! Guess a different one.")
+else:
+    print("\nThat is not a letter! Guess a different one.")
+if lives == 0:  # marks the end of the game
     print(hangmanLivesVisual[lives])  # prints the hangman lives visual
-    print("Game over. You're dead. The word was: " + word)  # prints the word that the user was trying to guess
-  else:
-    print("You win! The word was: " + word)  # prints the word that the user guessed correctly
-    
+    print("Game over. You're dead. The word was: " + word)
+    print("You win! The word was: " + word)
+
 if __name__ == "__main__":
-  hangmanGame()
+    hangmanGame()
